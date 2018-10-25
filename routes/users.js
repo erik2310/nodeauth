fe9772902d5ec264fa0aad15b7382d16429d63f6
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-10-25 02:22:48 +0200

    Dagens arbejde.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {title: 'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
 });
 
 module.exports = router;
